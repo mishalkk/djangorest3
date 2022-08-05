897817317eadc7df0247e6ddf23a04bd745cde9a
mishalkk <mishal.kk@simelabs.com>
2022-08-05 18:42:27 +0530

    user registration

--- django_api/settings.py
+++ django_api/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'debug_toolbar',
     'test_app'
 ]
 
@@ -47,9 +48,14 @@ MIDDLEWARE = [
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+INTERNAL_IPS = (
+    "127.0.0.1",
+)
 
 ROOT_URLCONF = 'django_api.urls'
 
--- django_api/urls.py
+++ django_api/urls.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.urls import path, include
 from test_app.views import Simple
 from rest_framework.routers import DefaultRouter
+from django.conf import settings
 
 router = DefaultRouter()
 router.register("simple-viewset", Simple)
@@ -10,3 +11,11 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include(router.urls)),
 ]
+
+
+if settings.DEBUG:
+    import debug_toolbar
+
+    urlpatterns = [
+        path('__debug__/', include(debug_toolbar.urls))
+    ] + urlpatterns
